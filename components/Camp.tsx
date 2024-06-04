4ec8cc9703a1c8efd6843ced3be6968c84e76794
Aryanxon <168644067+Aryanxon@users.noreply.github.com>
2024-06-04 17:21:33 +0530

    .

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -10,7 +10,7 @@ interface CampProps{
 
 const CampSite = ({backgroundImage, title, Subtitle, peoplejoined}:CampProps) =>{
   return(
-    <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+    <div className={`h-full w-full min-w-[1100px] ${./backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
       <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
         <div className="flexCenter gap-4">
           <div className="rounded-full bg-green-50 p-4">
@@ -53,12 +53,12 @@ const Camp = () => {
     <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h[640px]">
         <CampSite
-        backgroundImage="bg-[url(./img-1.png)]"
+        backgroundImage="bg-bg-img-1.png"
         title="Putuk Truno Camp"
         Subtitle="Prigrn Pasuruan"
         peoplejoined="50+ Joined"/>
         <CampSite
-        backgroundImage="bg-[url(./img-2.png)]"
+        backgroundImage="bg-bg-img-2.png"
         title="Mountain View"
         Subtitle="Prigrn Pasuruan"
         peoplejoined="60+ Joined"/>
